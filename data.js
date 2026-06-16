3568d0e9d1ba8192a661bdf0144c82762f7929ea
almehedi0304 <jhpi133826@gmail.com>
2026-06-17 01:48:49 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+const family = [
+  {
+    name: "দাদা",
+    image: "https://via.placeholder.com/80",
+    generation: 1
+  },
+  {
+    name: "দাদী",
+    image: "https://via.placeholder.com/80",
+    generation: 1
+  },
+  {
+    name: "মেহেদী (বাবা)",
+    image: "https://via.placeholder.com/80",
+    generation: 2
+  },
+  {
+    name: "সাওদা (মা)",
+    image: "https://via.placeholder.com/80",
+    generation: 2
+  },
+  {
+    name: "সায়ন",
+    image: "https://via.placeholder.com/80",
+    generation: 3
+  }
+];
