2a86b97cf26a256b8df182c1b247c4097c2ab7ae
almehedi0304 <jhpi133826@gmail.com>
2026-06-17 10:43:14 +0600

    Update data.js

--- data.js
+++ data.js
@@ -1,42 +1,10 @@
 const family = [
-  {
-    id: 1,
-    name: "দাদা",
-    gender: "male",
-    father: null,
-    mother: null,
-    image: "https://via.placeholder.com/100"
-  },
-  {
-    id: 2,
-    name: "দাদী",
-    gender: "female",
-    father: null,
-    mother: null,
-    image: "https://via.placeholder.com/100"
-  },
-  {
-    id: 3,
-    name: "মেহেদী (বাবা)",
-    gender: "male",
-    father: 1,
-    mother: 2,
-    image: "https://via.placeholder.com/100"
-  },
-  {
-    id: 4,
-    name: "সাওদা (মা)",
-    gender: "female",
-    father: null,
-    mother: null,
-    image: "https://via.placeholder.com/100"
-  },
-  {
-    id: 5,
-    name: "সায়ন",
-    gender: "male",
-    father: 3,
-    mother: 4,
-    image: "https://via.placeholder.com/100"
-  }
+  { id: 1, name: "দাদা", father: null, mother: null },
+  { id: 2, name: "দাদী", father: null, mother: null },
+
+  { id: 3, name: "মেহেদী (বাবা)", father: 1, mother: 2 },
+  { id: 4, name: "সাওদা (মা)", father: null, mother: null },
+
+  { id: 5, name: "সায়ন", father: 3, mother: 4 },
+  { id: 6, name: "সাবা", father: 3, mother: 4 }
 ];
