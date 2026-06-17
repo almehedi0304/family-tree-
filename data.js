24b7290fed6014707e1aad1de4046e2cf0e95767
almehedi0304 <jhpi133826@gmail.com>
2026-06-17 08:27:33 +0300

    Update data.js

--- data.js
+++ data.js
@@ -3,5 +3,6 @@ const family = [
   { id: 2, name: "দাদী", father: null, mother: null },
   { id: 3, name: "মেহেদী", father: 1, mother: 2 },
   { id: 4, name: "সাওদা", father: null, mother: null },
-  { id: 5, name: "সায়ন", father: 3, mother: 4 }
+  { id: 5, name: "সায়ন", father: 3, mother: 4 },
+  { id: 6, name: "সাবা", father: 3, mother: 4 }
 ];
