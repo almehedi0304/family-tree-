503dec07d1f503e831fdd0ed5b472851dd044c84
almehedi0304 <jhpi133826@gmail.com>
2026-06-17 10:15:04 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const tree = document.getElementById("tree");
 
+function getPerson(id) {
+  return family.find(person => person.id === id);
+}
+
 function renderTree() {
   let html = "";
 
   family.forEach(person => {
+
+    let fatherName = "";
+    let motherName = "";
+
+    if (person.father) {
+      fatherName = getPerson(person.father)?.name || "";
+    }
+
+    if (person.mother) {
+      motherName = getPerson(person.mother)?.name || "";
+    }
+
     html += `
       <div class="node">
-        <img src="${person.image}">
-        <p>${person.name}</p>
+        <h3>${person.name}</h3>
+
+        <p>বাবা: ${fatherName || "অজানা"}</p>
+        <p>মা: ${motherName || "অজানা"}</p>
       </div>
     `;
   });
