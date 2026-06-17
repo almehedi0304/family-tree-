ca1eb6263c119f36608285693f75fc08dd34e892
almehedi0304 <jhpi133826@gmail.com>
2026-06-17 09:58:26 +0600

    Update data.js

--- data.js
+++ data.js
@@ -1,27 +1,21 @@
 const family = [
   {
-    name: "দাদা",
-    image: "https://via.placeholder.com/80",
-    generation: 1
+    id: 1,
+    name: "মেহেদী",
+    father: null,
+    mother: null
   },
   {
-    name: "দাদী",
-    image: "https://via.placeholder.com/80",
-    generation: 1
+    id: 2,
+    name: "সাওদা",
+    father: null,
+    mother: null
   },
   {
-    name: "মেহেদী (বাবা)",
-    image: "https://via.placeholder.com/80",
-    generation: 2
-  },
-  {
-    name: "সাওদা (মা)",
-    image: "https://via.placeholder.com/80",
-    generation: 2
-  },
-  {
+    id: 3,
     name: "সায়ন",
-    image: "https://via.placeholder.com/80",
-    generation: 3
+    father: 1,
+    mother: 2
   }
+];
 ];
