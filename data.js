2ef5debdf70bc0434f8049bbed9a8daa090f4c07
almehedi0304 <jhpi133826@gmail.com>
2026-06-17 10:03:25 +0600

    Update data.js

--- data.js
+++ data.js
@@ -1,10 +1,18 @@
 {
-  id: 25,
+  id: 10,
   name: "আব্দুল করিম",
-  gender: "male",
+  father: null,
+  mother: null
+},
+{
+  id: 11,
+  name: "রহিমা খাতুন",
+  father: null,
+  mother: null
+},
+{
+  id: 1,
+  name: "মেহেদী",
   father: 10,
-  mother: 11,
-  birthYear: 1965,
-  photo: "images/karim.jpg"
+  mother: 11
 }
-];
